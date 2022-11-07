6a5ad52990cd6cef22b4c51a49beef1c9fd8b258
divij kathuria <divijkathuria51714@gmail.com>
2022-11-07 10:51:36 +0530

    pb 53

---
+++ maxSubarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum = INT_MIN;
+        int cursum = 0;
+        for(int i=0;i<nums.size();i++){
+            cursum = cursum+nums[i];
+            if(cursum>maxsum){
+                maxsum = cursum;
+            }
+            if(cursum<0){
+                cursum = 0;
+            }
+        }
+        return maxsum;
+    }
+};
