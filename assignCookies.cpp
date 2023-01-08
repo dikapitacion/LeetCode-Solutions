23ceec2161cbd67bbed6d4823dda70a531dd9c81
divij kathuria <divijkathuria51714@gmail.com>
2023-01-08 18:51:28 +0530

    pb 455

---
+++ assignCookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int ans = 0,i=0,j=0;
+        while(i<g.size() && j<s.size()){
+            if(g[i]>s[j]){
+                j++;
+            }
+            else{
+                ans++;
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
