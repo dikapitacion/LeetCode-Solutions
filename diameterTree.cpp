243a2cacdcd9b9a0b1955bb3fa11ceb6f29f510f
Divij <divijkathuria51714@gmail.com>
2024-01-08 10:59:39 +0530

    pb 543 updated TC

--- diameterTree.cpp
+++ diameterTree.cpp
@@ -11,6 +11,25 @@
  */
 class Solution {
 public:
+    pair<int,int> dFast(TreeNode* root){
+        if(!root){
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+
+        pair<int,int> left = dFast(root->left);
+        pair<int,int> right = dFast(root->right);
+
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second+right.second;
+
+        pair<int,int> ans;
+        ans.first = max(op1,max(op2,op3));
+        ans.second = max(left.second,right.second)+1;
+
+        return ans;
+    }
     int height(TreeNode* root){
         if(!root){
             return 0;
@@ -20,13 +39,16 @@ public:
         return max(left,right)+1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root){
-            return 0;
-        }
-        int left = diameterOfBinaryTree(root->left);
-        int right = diameterOfBinaryTree(root->right);
-        int both = height(root->left)  + height(root->right);
-        int ans = max(left,right);
-        return max(ans,both);
+        // if(!root){
+        //     return 0;
+        // }
+        // int left = diameterOfBinaryTree(root->left);
+        // int right = diameterOfBinaryTree(root->right);
+        // int both = height(root->left) + height(root->right);
+        // int ans = max(left,right);
+        // return max(ans,both);
+
+        pair<int,int> ans = dFast(root);
+        return ans.first;
     }
 };
