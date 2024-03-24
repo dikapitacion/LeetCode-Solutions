78f7dc0ba0e3c97e5054897e439a1f05e9154a4b
Divij <divijkathuria51714@gmail.com>
2024-03-24 13:34:45 +0530

    pb 287

---
+++ findDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr(n,0);
+
+        for(int i=0;i<n;i++){
+            arr[nums[i]]++;
+            if(arr[nums[i]]>=2){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
